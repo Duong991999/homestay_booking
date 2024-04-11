d26e2e8820761043eb0777d3de01347abae27294
Long <vuongduylong0@gmail.com>
2024-04-11 21:43:25 +0700

    new

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,49 +1,12 @@
 <template>
     <div>
-        <nav class="navbar-parent">
-            <div class="navbar-title">
-                <router-link class="navbar-brand" :to="{ name: 'home' }"
-                    >HomestayBooking</router-link
-                >
-                <div class="navbar-login">
-                    <router-link
-                        :to="{ name: 'basket' }"
-                        class="nav-link-1"
-                        style="text-decoration: none"
-                    >
-                        Basket
-                        <span v-if="itemsInBasket" class="badge badge-secondary">{{
-                            itemsInBasket
-                        }}</span>
-                    </router-link>
-
-                    <router-link
-                        :to="{ name: 'register' }"
-                        class="nav-link-1"
-                        v-if="!isLoggedIn"
-                        style="text-decoration: none"
-                        >Register</router-link
-                    >
-
-                    <router-link
-                        :to="{ name: 'login' }"
-                        class="nav-link-1"
-                        v-if="!isLoggedIn"
-                        style="text-decoration: none"
-                        >Sign-in</router-link
-                    >
-
-                    <a
-                        class="nav-link-1"
-                        href="#"
-                        @click.prevent="logout"
-                        style="text-decoration: none"
-                        v-if="isLoggedIn"
-                        >Logout</a
-                    >
-                </div>
-            </div>
-
+        <nav
+            class="navbar navbar-custom navbar-expand-lg justify-content-between fixed-top text-center"
+        >
+            <router-link class="navbar-brand mb-0 h1" :to="{ name: 'home' }">
+                HomestayBooking</router-link
+            >
+            <!-- Dropdowm -->
             <div class="narbar-d">
                 <div class="nav-item dropdown" v-if="!isLoggedIn">
                     <a
@@ -55,10 +18,9 @@
                         aria-haspopup="true"
                         aria-expanded="false"
                     >
-                        User Menu
+                        Lưu trú
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <!-- You can add more dropdown items here -->
                         <router-link :to="{ name: 'profile' }" class="dropdown-item"
                             >Profile</router-link
                         >
@@ -74,10 +36,9 @@
                         aria-haspopup="true"
                         aria-expanded="false"
                     >
-                        User Menu
+                        Chuyến bay
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <!-- You can add more dropdown items here -->
                         <router-link :to="{ name: 'profile' }" class="dropdown-item"
                             >Profile</router-link
                         >
@@ -93,17 +54,118 @@
                         aria-haspopup="true"
                         aria-expanded="false"
                     >
-                        User Menu
+                        Chuyến bay + khách sạn
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <!-- You can add more dropdown items here -->
                         <router-link :to="{ name: 'profile' }" class="dropdown-item"
                             >Profile</router-link
                         >
                     </div>
                 </div>
             </div>
+
+            <div class="navbar-login">
+                <router-link
+                    :to="{ name: 'basket' }"
+                    class="nav-link-1"
+                    style="text-decoration: none"
+                >
+                    Basket
+                    <span v-if="itemsInBasket" class="badge badge-secondary">{{
+                        itemsInBasket
+                    }}</span>
+                </router-link>
+
+                <router-link
+                    :to="{ name: 'register' }"
+                    class="nav-link-1"
+                    v-if="!isLoggedIn"
+                    style="text-decoration: none"
+                    >Register</router-link
+                >
+
+                <router-link
+                    :to="{ name: 'login' }"
+                    class="nav-link-1"
+                    v-if="!isLoggedIn"
+                    style="text-decoration: none"
+                    >Sign-in</router-link
+                >
+
+                <a
+                    class="nav-link-1"
+                    href="#"
+                    @click.prevent="logout"
+                    style="text-decoration: none"
+                    v-if="isLoggedIn"
+                    >Logout</a
+                >
+            </div>
+
+            <!--Check form-->
+            <!-- <form class="form-input">
+                <input type="text" id="fname" name="firstname" placeholder="Bạn muốn đi đâu" />
+                <div>
+                    <label for="example-datepicker">Choose a date</label>
+                    <b-form-datepicker
+                        id="example-datepicker"
+                        v-model="value"
+                        class="mb-2"
+                    ></b-form-datepicker>
+                    <p>Value: '{{ value }}'</p>
+                </div>
+                <input type="text" id="fname" name="firstname" placeholder="Bạn muốn đi đâu" />
+            </form> -->
+            <!--end-->
         </nav>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img
+                        class="d-block w-100"
+                        src="assets/image/hinh-anh-bat-mi-y-tuong-thiet-ke-homestay-doc-dao-hut-khach-so-1.jpg"
+                        alt="First slide"
+                    />
+                </div>
+                <div class="carousel-item">
+                    <img
+                        class="d-block w-100"
+                        src="assets/image/hinh-anh-bat-mi-y-tuong-thiet-ke-homestay-doc-dao-hut-khach-so-1.jpg"
+                        alt="Second slide"
+                    />
+                </div>
+                <div class="carousel-item">
+                    <img
+                        class="d-block w-100"
+                        src="assets\image\nha-go-homestay-1.jpg"
+                        alt="Third slide"
+                    />
+                </div>
+            </div>
+            <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+            >
+                <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span> -->
+            </a>
+            <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+            >
+                <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span> -->
+            </a>
+        </div>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
             <router-view></router-view>
@@ -141,18 +203,40 @@ export default {
                 this.$store.dispatch('logout');
             }
         },
+        initializeCarousel() {
+            $('.carousel').carousel({
+                interval: 2000,
+            });
+        },
+    },
+    mounted() {
+        // Add mounted hook to handle scroll event
+        const navEl = document.querySelector('.navbar');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY >= 200) {
+                navEl.classList.add('navbar-scrolled');
+            } else {
+                navEl.classList.remove('navbar-scrolled');
+            }
+        });
+
+        this.initializeCarousel();
     },
 };
 </script>
 
 <style scoped>
-.navbar-parent {
-    background-color: #003b95;
+.navbar {
+    transition: all 0.5s;
 }
-.navbar-brand {
-    color: aliceblue;
-    margin: 16px 16px 16px 30px;
+.navbar-custom {
+    height: 50px;
+    border-bottom: 0.5px inset #50555e;
+}
 
+.navbar-brand {
+    font-size: 30px;
+    color: rgb(21, 5, 245);
     justify-content: center;
 }
 .navbar-login {
@@ -177,12 +261,36 @@ export default {
     align-items: center;
     justify-content: space-between;
 }
-
+.navbar-scrolled {
+    background-color: rgb(249, 249, 249);
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+.carousel-item {
+    height: 80vh;
+    min-height: 300px;
+    background: no-repeat scroll center scroll;
+}
 .dropdown {
     display: inline-block;
+    background-color: aliceblue;
     flex-direction: row;
     border: 1px solid white;
     border-radius: 20px;
     margin: 16px;
+}
+.text-content {
+    color: white;
+    font-size: 30px;
+    font-weight: 600;
+    padding: 16px;
+}
+.form-input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
 }
 </style>
