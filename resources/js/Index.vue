19435e4de8d7d76ca91b4ecaa404d9ed71a01522
Long <vuongduylong0@gmail.com>
2024-05-13 00:31:53 +0700

    new

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
+        <!--Thanh nav-->
         <nav
             class="navbar navbar-custom navbar-expand-lg justify-content-between fixed-top text-center"
+            style="height: 80px"
         >
             <router-link class="navbar-brand mb-0 h1" :to="{ name: 'home' }">
                 <img
@@ -14,7 +16,7 @@
             </router-link>
 
             <!-- Dropdowm -->
-            <div class="narbar-d">
+            <!-- <div class="narbar-d">
                 <div class="nav-item dropdown" v-if="!isLoggedIn">
                     <a
                         class="nav-link dropdown-toggle"
@@ -69,7 +71,7 @@
                         >
                     </div>
                 </div>
-            </div>
+            </div> -->
 
             <div class="navbar-login">
                 <router-link
@@ -109,74 +111,126 @@
                 >
             </div>
         </nav>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <!--carousel---->
+        <div class="container position-relative" style="top: 50px">
+            <div class="row g-4 g-lg-5" style="margin-top: 48px">
+                <div class="col-lg-6 position-relative mb-4 mb-md-0">
+                    <h1 class="mb-4 mt-md-5 display-5 font-weight-bold">
+                        Find the top <br /><span class="position-relative z-index-9">
+                            Homestay nearby.
+                            <span
+                                class="position-absolute top-50 start-50 translate-middle z-index-n1 d-none d-md-block mt-4"
+                                style="top: 0px; fill: antiquewhite"
+                            >
+                                <svg width="390.5px" height="21.5px">
+                                    <path
+                                        class="fill-primary opacity-7"
+                                        d="M409.9,2.6c-9.7-0.6-19.5-1-29.2-1.5c-3.2-0.2-6.4-0.2-9.7-0.3c-7-0.2-14-0.4-20.9-0.5 c-3.9-0.1-7.8-0.2-11.7-0.3c-1.1,0-2.3,0-3.4,0c-2.5,0-5.1,0-7.6,0c-11.5,0-23,0-34.5,0c-2.7,0-5.5,0.1-8.2,0.1 c-6.8,0.1-13.6,0.2-20.3,0.3c-7.7,0.1-15.3,0.1-23,0.3c-12.4,0.3-24.8,0.6-37.1,0.9c-7.2,0.2-14.3,0.3-21.5,0.6 c-12.3,0.5-24.7,1-37,1.5c-6.7,0.3-13.5,0.5-20.2,0.9C112.7,5.3,99.9,6,87.1,6.7C80.3,7.1,73.5,7.4,66.7,8 C54,9.1,41.3,10.1,28.5,11.2c-2.7,0.2-5.5,0.5-8.2,0.7c-5.5,0.5-11,1.2-16.4,1.8c-0.3,0-0.7,0.1-1,0.1c-0.7,0.2-1.2,0.5-1.7,1 C0.4,15.6,0,16.6,0,17.6c0,1,0.4,2,1.1,2.7c0.7,0.7,1.8,1.2,2.7,1.1c6.6-0.7,13.2-1.5,19.8-2.1c6.1-0.5,12.3-1,18.4-1.6 c6.7-0.6,13.4-1.1,20.1-1.7c2.7-0.2,5.4-0.5,8.1-0.7c10.4-0.6,20.9-1.1,31.3-1.7c6.5-0.4,13-0.7,19.5-1.1c2.7-0.1,5.4-0.3,8.1-0.4 c10.3-0.4,20.7-0.8,31-1.2c6.3-0.2,12.5-0.5,18.8-0.7c2.1-0.1,4.2-0.2,6.3-0.2c11.2-0.3,22.3-0.5,33.5-0.8 c6.2-0.1,12.5-0.3,18.7-0.4c2.2-0.1,4.4-0.1,6.7-0.1c11.5-0.1,23-0.2,34.6-0.4c7.2-0.1,14.4-0.1,21.6-0.1c12.2,0,24.5,0.1,36.7,0.1 c2.4,0,4.8,0.1,7.2,0.2c6.8,0.2,13.5,0.4,20.3,0.6c5.1,0.2,10.1,0.3,15.2,0.4c3.6,0.1,7.2,0.4,10.8,0.6c10.6,0.6,21.1,1.2,31.7,1.8 c2.7,0.2,5.4,0.4,8,0.6c2.9,0.2,5.8,0.4,8.6,0.7c0.4,0.1,0.9,0.2,1.3,0.3c1.1,0.2,2.2,0.2,3.2-0.4c0.9-0.5,1.6-1.5,1.9-2.5 c0.6-2.2-0.7-4.5-2.9-5.2c-1.9-0.5-3.9-0.7-5.9-0.9c-1.4-0.1-2.7-0.3-4.1-0.4c-2.6-0.3-5.2-0.4-7.9-0.6 C419.7,3.1,414.8,2.9,409.9,2.6z"
+                                    ></path></svg></span
+                        ></span>
+                    </h1>
+                    <p class="mb-4">
+                        We bring you not only a stay option, but an experience in your budget to
+                        enjoy the luxury.
+                    </p>
+                </div>
+
+                <div class="col-lg-6 position-relative z-0" style="box-sizing: border-box">
+                    <div
+                        id="carouselExampleIndicators"
+                        class="carousel slide carousel-fade"
+                        data-ride="carousel"
+                    >
+                        <ol class="carousel-indicators">
+                            <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
-            </ol>
-            <div class="carousel-inner">
-                <div
-                    v-for="(file, index) in list_home_screen"
-                    :class="'carousel-item' + (index ? ' active' : '')"
-                >
-                    <img
-                        class="d-block w-100"
-                        :src="`assets/image/slide_home_screens/${file}`"
-                        :alt="`Slide ${index}`"
-                    />
-                </div>
-            </div>
-            <a
-                class="carousel-control-prev"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="prev"
-            >
-                <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span> -->
-            </a>
-            <a
-                class="carousel-control-next"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="next"
-            >
-                <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span> -->
-            </a>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 bg-white shadow p-4 rounded">
-                    <form>
-                        <div class="row align-items-end">
-                            <div class="col-lg-3">
-                                <label class="form-label" style="font-weight: 500">Địa điểm</label>
-                                <input
-                                    type="place"
-                                    class="form-control shaw-none"
-                                    placeholder="Nơi bạn muốn đến"
+                        </ol>
+                        <div class="carousel-inner">
+                            <div
+                                v-for="(file, index) in list_home_screen"
+                                :class="'carousel-item' + (index ? ' active' : '')"
+                            >
+                                <img
+                                    class="d-block w-100"
+                                    :src="`assets/image/slide_home_screens/${file}`"
+                                    :alt="`Slide ${index}`"
+                                    style="border-radius: 10px; height: 600px"
                                 />
                             </div>
-                            <div class="col-lg-3">
-                                <label class="form-label" style="font-weight: 500"
-                                    >Ngày nhận phòng</label
-                                >
-                                <input type="date" class="form-control shaw-none" />
+                        </div>
+                        <a
+                            class="carousel-control-prev"
+                            href="#carouselExampleIndicators"
+                            role="button"
+                            data-slide="prev"
+                        >
+                            <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span> -->
+                        </a>
+                        <a
+                            class="carousel-control-next"
+                            href="#carouselExampleIndicators"
+                            role="button"
+                            data-slide="next"
+                        >
+                            <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span> -->
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--Form-->
+            <div class="row position-absolute" style="top: 500px; z-index: 1">
+                <div class="col-xl-10 position-relative z-1 mt-n3 mt-xl-n9">
+                    <h6 class="d-none d-xl-block mb-3">Check Availability</h6>
+                    <form
+                        class="card shadow rounded-3 position-relative p-4 pe-md-5 pb-5 pb-md-4"
+                        style="width: 900px; border-radius: 30px"
+                    >
+                        <div class="row g-4 align-items-center">
+                            <div class="input col-lg-4">
+                                <input
+                                    class="input__field"
+                                    type="text"
+                                    v-model="inputValue"
+                                    @focus="isFocused = true"
+                                    @blur="isFocused = false"
+                                />
+                                <span class="input__label">Địa điểm</span>
                             </div>
                             <div class="col-lg-3">
-                                <label class="form-label" style="font-weight: 500"
-                                    >Ngày trả phòng</label
-                                >
-                                <input type="date" class="form-control shaw-none" />
+                                <input
+                                    class="input__field"
+                                    type="date"
+                                    v-model="inputValue"
+                                    @focus="isFocused = true"
+                                    @blur="isFocused = false"
+                                />
+                                <span class="input__label">Ngày nhận phòng</span>
                             </div>
-                            <div class="col-lg-1">
+                            <div class="col-lg-3">
+                                <input
+                                    class="input__field"
+                                    type="date"
+                                    v-model="inputValue"
+                                    @focus="isFocused = true"
+                                    @blur="isFocused = false"
+                                />
+                                <span class="input__label">Ngày trả phòng</span>
+                            </div>
+
+                            <div class="col-lg-1 position-absolute" style="left: 850px">
                                 <button
-                                    class="btn btn-primary text-white shadow-none custom-bg"
+                                    class="btn btn-primary custom-bg border-0"
                                     type="submit"
+                                    style="border-radius: 50px; width: 4rem; height: 4rem"
                                 >
-                                    Tìm kiếm
+                                    <img
+                                        src="assets/image/icon/search.png"
+                                        alt="search "
+                                        style="width: 100%"
+                                    />
                                 </button>
                             </div>
                         </div>
@@ -185,7 +239,7 @@
             </div>
         </div>
         <!-- Location noi bat  -->
-        <div class="location_choice">
+        <div class="location_choice pb-2 pb-lg-5">
             <div>
                 <span
                     style="
@@ -337,6 +391,8 @@ export default {
             // carouselItems: [], // Assuming carouselItems is an array of items
             scrollPosition: 0,
             cardWidth: 0,
+            inputValue: '',
+            isFocused: false,
         };
     },
     computed: {
@@ -391,7 +447,7 @@ export default {
         // Add mounted hook to handle scroll event
         const navEl = document.querySelector('.navbar');
         window.addEventListener('scroll', () => {
-            if (window.scrollY >= 200) {
+            if (window.scrollY >= 30) {
                 navEl.classList.add('navbar-scrolled');
             } else {
                 navEl.classList.remove('navbar-scrolled');
@@ -444,8 +500,8 @@ export default {
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 }
 .carousel-item {
-    height: 40vh;
-    min-height: 450px;
+    height: 600px;
+    min-height: 600px;
     background: no-repeat scroll center scroll;
 }
 .dropdown {
@@ -463,14 +519,14 @@ export default {
     padding: 16px;
 }
 .custom-bg {
-    background-color: #4882da;
+    background-color: #f87f06d2;
 }
 .custom-bg:hover {
     background-color: #04419c;
 }
 .location_choice {
-    margin: 64px auto;
-    width: 90%;
+    margin: 200px auto;
+    width: 80%;
 }
 @media (min-width: 500px) {
     .carousel-inner-location {
@@ -482,11 +538,12 @@ export default {
     .carousel-item-location {
         display: inline-block;
         margin-right: 0;
-        flex: 0 0 33.3%;
+        flex: 0 0 25%;
     }
 }
 .card {
-    margin: 0 0.5em;
+    width: 280px;
+    margin: 0 1em;
     border: none;
 }
 .carousel-control-prev-location,
@@ -497,5 +554,34 @@ export default {
     border-radius: 50%;
     top: 50%;
     transform: translateY(-50%);
+}
+.input {
+    position: relative;
+}
+
+.input__label {
+    position: absolute;
+    left: 10px; /* Điều chỉnh khoảng cách từ mép trái */
+    top: -10px; /* Điều chỉnh khoảng cách từ mép trên */
+    background: white; /* Đổi màu nền nếu cần */
+    padding: 0 5px; /* Điều chỉnh padding nếu cần */
+    font-weight: bold;
+    color: #888; /* Đổi màu chữ nếu cần */
+    z-index: 1;
+}
+
+.input__field {
+    box-sizing: border-box;
+    width: 100%;
+    border: 2px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+}
+
+.input__field:focus + .input__label,
+.input__field:not(:placeholder-shown) + .input__label {
+    top: -10px; /* Điều chỉnh vị trí khi input field được focus */
+    font-size: 12px; /* Điều chỉnh kích thước chữ khi input field được focus */
+    color: #333; /* Đổi màu chữ khi input field được focus */
 }
 </style>
