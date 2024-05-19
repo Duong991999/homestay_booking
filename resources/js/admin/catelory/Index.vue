f9fba9ae9fdc54022012c1a989db8c1b7e8d409f
Long <vuongduylong0@gmail.com>
2024-05-19 11:49:31 +0700

    new

--- resources/js/admin/catelory/Index.vue
+++ resources/js/admin/catelory/Index.vue
@@ -4,7 +4,12 @@
             <div class="container">
                 <div class="bg-light rounded p-3 d-none d-lg-block">
                     <div class="row row-cols-7 g-4">
-                        <div class="col-sm-8"><h6 class="mb-0">Name</h6></div>
+                        <div class="col-sm-2 d-flex justify-content-center">
+                            <h6 class="mb-0">ID</h6>
+                        </div>
+                        <div class="col-sm-6 d-flex justify-content-center">
+                            <h6 class="mb-0">Name</h6>
+                        </div>
                         <div class="col-sm-4 d-flex justify-content-center">
                             <h6 class="mb-0">Action</h6>
                         </div>
@@ -15,11 +20,20 @@
                     :key="index"
                     class="row row-cols-xl-7 align-items-lg-center border-bottom g-4 px-2 py-4"
                 >
-                    <div class="col-sm-8">
-                        <small class="d-block d-lg-none">Name</small>
+                    <div class="col-sm-2 d-flex justify-content-center">
+                        <small class="d-block d-lg-none">ID</small>
                         <div class="d-flex align-items-center">
                             <div class="avatar avatar-xs flex-shrink-0"></div>
                             <div class="ms-2">
+                                <h6 class="mb-0 fw-light">{{ item.id }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 d-flex justify-content-center">
+                        <small class="d-block d-lg-none">Name</small>
+                        <div class="d-flex align-items-center">
+                            <div class="avatar avatar-xs flex-shrink-0"></div>
+                            <div class="ms-2 d-flex justify-content-center">
                                 <h6 class="mb-0 fw-light">{{ item.name }}</h6>
                             </div>
                         </div>
@@ -62,10 +76,21 @@
 export default {
     data() {
         return {
-            items: [{ name: 'Lori Stevens' }, { name: 'John Doe' }, { name: 'Jane Smith' }],
+            items: [],
         };
     },
+    mounted() {
+        this.fetchData();
+    },
     methods: {
+        async fetchData() {
+            try {
+                const response = await axios.post('/api/admin/category/index'); 
+                this.items = response.data;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        },
         showItem(item) {
             console.log('Show', item);
         },
