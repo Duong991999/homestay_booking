88aba2d853f34b2adfdfab73b9cbc43560e4dc71
Long <vuongduylong0@gmail.com>
2024-05-19 09:47:31 +0700

    new

---
+++ resources/js/bookable/ImageList.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="row g-2">
+        <div class="col-md-6 image-item">
+            <a class="glightbox" href="assets/image/slide_home_screens/_1.jpg">
+                <div
+                    class="card card-grid-lg card-element-hover card-overlay-hover overflow-hidden border-0"
+                    style="
+                        background-image: url('assets/image/slide_home_screens/_1.jpg');
+                        background-position: left center;
+                        background-size: cover;
+                        height: 400px;
+                    "
+                >
+                    <!-- <div class="hover-element position-absolute w-100 h-100">
+                                <svg
+                                    width="1em"
+                                    height="1em"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    role="img"
+                                    focusable="false"
+                                    class="h4 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                                >
+                                    <path
+                                        d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5"
+                                    ></path>
+                                </svg>
+                            </div> -->
+                </div>
+            </a>
+        </div>
+        <div class="col-md-6 image-item">
+            <div class="row g-2">
+                <div class="col-12">
+                    <a class="glightbox" href="assets/image/slide_home_screens/_1.jpg">
+                        <div
+                            class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden border-0"
+                            style="
+                                background-image: url('assets/image/slide_home_screens/_1.jpg');
+                                background-position: left center;
+                                background-size: cover;
+                                height: 240px;
+                            "
+                        >
+                            <!-- <div class="hover-element position-absolute w-100 h-100">
+                                        <svg
+                                            width="1em"
+                                            height="1em"
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            role="img"
+                                            focusable="false"
+                                            class="h4 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                                        ></svg>
+                                    </div> -->
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6" style="margin-top: 8px">
+                    <a class="glightbox" href="assets/image/slide_home_screens/_1.jpg">
+                        <div
+                            class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden border-0"
+                            style="
+                                background-image: url('assets/image/slide_home_screens/_1.jpg');
+                                background-position: left center;
+                                background-size: cover;
+                                height: 150px;
+                            "
+                        >
+                            <!-- <div class="hover-element position-absolute w-100 h-100">
+                                        <svg
+                                            width="1em"
+                                            height="1em"
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            role="img"
+                                            focusable="false"
+                                            class="h4 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                                        ></svg>
+                                    </div> -->
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6" style="margin-top: 8px; padding-left: 0px">
+                    <div
+                        class="card card-grid-sm overflow-hidden"
+                        style="
+                            background-image: url('assets/image/slide_home_screens/_1.jpg');
+                            background-position: left center;
+                            background-size: cover;
+                            height: 150px;
+                        "
+                    >
+                        <div class="bg-overlay bg-dark opacity-7"></div>
+                        <a
+                            class="glightbox stretched-link z-index-9"
+                            href="assets/image/slide_home_screens/_1.jpg"
+                        ></a>
+                        <a class="glightbox" href="assets/image/slide_home_screens/_1.jpg"></a>
+                        <a class="glightbox" href="assets/image/slide_home_screens/_1.jpg"></a>
+                        <div class="card-img-overlay d-flex h-100 w-100">
+                            <h6 class="card-title m-auto fw-light text-decoration-underline">
+                                <a href="#" class="text-white">View all</a>
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script></script>
+<style scoped>
+.list {
+    width: 100%;
+    height: 408px;
+}
+.image-item {
+    padding-left: 8px;
+    padding-right: 8px;
+    margin-top: 8px;
+}
+.card {
+    border-radius: 20px;
+}
+</style>
