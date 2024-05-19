59a641292f7ae5c3ba38ebdc536ca9b15913711d
Long <vuongduylong0@gmail.com>
2024-05-19 11:16:48 +0700

    new FE

---
+++ resources/js/admin/catelory/Index.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="card w-100" style="margin-top: 100px">
+        <div class="card-body">
+            <div class="container">
+                <div class="bg-light rounded p-3 d-none d-lg-block">
+                    <div class="row row-cols-7 g-4">
+                        <div class="col-sm-8"><h6 class="mb-0">Name</h6></div>
+                        <div class="col-sm-4 d-flex justify-content-center">
+                            <h6 class="mb-0">Action</h6>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    v-for="(item, index) in items"
+                    :key="index"
+                    class="row row-cols-xl-7 align-items-lg-center border-bottom g-4 px-2 py-4"
+                >
+                    <div class="col-sm-8">
+                        <small class="d-block d-lg-none">Name</small>
+                        <div class="d-flex align-items-center">
+                            <div class="avatar avatar-xs flex-shrink-0"></div>
+                            <div class="ms-2">
+                                <h6 class="mb-0 fw-light">{{ item.name }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="row d-flex justify-content-around">
+                            <a
+                                aria-current="page"
+                                href="#"
+                                class="router-link-active router-link-exact-active btn btn-info mb-0"
+                                @click="showItem(item)"
+                            >
+                                Show
+                            </a>
+                            <a
+                                aria-current="page"
+                                href="#"
+                                class="router-link-active router-link-exact-active btn btn-warning mb-0"
+                                @click="updateItem(item)"
+                            >
+                                Update
+                            </a>
+                            <a
+                                aria-current="page"
+                                href="#"
+                                class="router-link-active router-link-exact-active btn btn-danger mb-0"
+                                @click="deleteItem(item)"
+                            >
+                                Delete
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [{ name: 'Lori Stevens' }, { name: 'John Doe' }, { name: 'Jane Smith' }],
+        };
+    },
+    methods: {
+        showItem(item) {
+            console.log('Show', item);
+        },
+        updateItem(item) {
+            console.log('Update', item);
+        },
+        deleteItem(item) {
+            console.log('Delete', item);
+        },
+    },
+};
+</script>
