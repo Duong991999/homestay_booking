58d720e6335d8a8ea86f89f248c0e98430eb72d0
Nguyễn Thế Tùng Dương <nttduong09091999@gmail.com>
2024-06-01 14:06:40 +0700

    paginate

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -17,7 +17,7 @@ class CategoryController extends Controller
     }
 
 	public function index(){
-		return $this->success($this->categoryRepo->paginateAll(10));
+		return $this->success($this->categoryRepo->paginateAll());
 	}
 
 	public function show($id){
