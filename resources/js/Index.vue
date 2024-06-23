576b518895ca35d16d6f357644e05fad8c5d833d
Long <vuongduylong0@gmail.com>
2024-06-23 12:59:45 +0700

    new booking

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -163,7 +163,7 @@ export default {
         // Add mounted hook to handle scroll event
         const navEl = document.querySelector('.navbar');
         window.addEventListener('scroll', () => {
-            if (window.scrollY >= 200) {
+            if (window.scrollY >= 100) {
                 navEl.classList.add('navbar-scrolled');
             } else {
                 navEl.classList.remove('navbar-scrolled');
